e48e0e05e89b38bfe6356db6de6afeba3cbe5d3f
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2020-07-07 13:31:03 -0700

    Update elv.rb

--- elv.rb
+++ elv.rb
@@ -3,10 +3,10 @@ class Elv < Formula
     desc "eluvio content fabric client"
     homepage "https://eluv.io"
     version "0.2.0"
-    url "https://github.com/eluv-io/homebrew-eluv-io/archive/#{version}.zip"
+    url "https://github.com/eluv-io/homebrew-eluv-io/archive/#$version.zip"
   
     bottle do
-        root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{version}"
+        root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#$version"
         sha256 "81d9ae0d1926ade7d3168d0fdac2cf196ce311cc815060ed51502bafdcce95f5" => :catalina
         sha256 "18f98e29a82058f068fbf42a7c30d98654eea39d10961bd014379df8b15c7233" => :mojave
         sha256 "93d7aab23ec8dce490d2368a645cd64fee0614bb4a1589e64af2502c311aa5c3" => :high_sierra
