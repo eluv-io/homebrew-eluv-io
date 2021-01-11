40094352c03cde7889aa67713868af94df8cce37
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2021-01-10 19:43:32 -0800

    test Catalina compatible with Big_sur

--- elv.rb
+++ elv.rb
@@ -7,6 +7,7 @@ class Elv < Formula
   
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
+        sha256 "d684a3e040d651d28ef6d476d9d78df978b1cc730dddb82c18cbdc4ba76f56e5" => :big_sur
         sha256 "d684a3e040d651d28ef6d476d9d78df978b1cc730dddb82c18cbdc4ba76f56e5" => :catalina
         sha256 "115fc39a19c63c90a9f94112541e4b884e0d3c4e1fedd341ca9399ab75d97c42" => :mojave
         sha256 "e93a1da31f9d5b7752a95b2757e5570065fd6ac441fd08ae06f6315cf740f81a" => :high_sierra
