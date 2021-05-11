30ad856f406c6af1d9f07a253d04e680376d1325
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2021-05-10 19:41:33 -0700

    Update elv.rb

--- elv.rb
+++ elv.rb
@@ -7,10 +7,10 @@ class Elv < Formula
     
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
-        sha256 "a0a1fbbb2f0f4bea56b284b5d3268b1f7313a0769bbaef7738c4398889557a8e" => :big_sur
-        sha256 "2698bdf38c9f6d6b90de76f37ac3d6585abbc5d7f261bd76440814fec87fda3b" => :catalina
-        sha256 "c2d1f5aaeb5fe2f4456984c91a1d7f17e73064c6e6f9e87cb84b2ef837e06140" => :mojave
-        sha256 "50b040f2199ef0b641fae6799976cb525df724be60ec30b6303c5c363026b8e9" => :high_sierra
+        sha256 big_sur:     "a0a1fbbb2f0f4bea56b284b5d3268b1f7313a0769bbaef7738c4398889557a8e"
+        sha256 catalina:    "2698bdf38c9f6d6b90de76f37ac3d6585abbc5d7f261bd76440814fec87fda3b"
+        sha256 high_sierra: "50b040f2199ef0b641fae6799976cb525df724be60ec30b6303c5c363026b8e9"
+        sha256 mojave:      "c2d1f5aaeb5fe2f4456984c91a1d7f17e73064c6e6f9e87cb84b2ef837e06140"
     end
     
     def install
