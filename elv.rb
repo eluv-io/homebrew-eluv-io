31f295b401f21f0206f00dcb8d8817c011bb9fe9
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2022-07-20 18:30:48 -0700

    Update elv.rb

--- elv.rb
+++ elv.rb
@@ -7,6 +7,7 @@ class Elv < Formula
     
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
+        sha256 catalina:    "60eadd836aa5843835e8c851e60857078e891c642717ad85fd134c1e40f42643"
         sha256 big_sur:     "60eadd836aa5843835e8c851e60857078e891c642717ad85fd134c1e40f42643"
         sha256 monterey:    "de0e322959d5ae376a3c9edcf1de20dc7e2e18b2970e082be2661179513867a9"
         sha256 arm64_big_sur: "44646e38a1df92fb73ca53cbaeb44348f54affa307f1208b6128ca4b1e2961e6"
