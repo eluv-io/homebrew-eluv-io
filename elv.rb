a7e2c0c3ec2ce2c3f1a0b333a0751bd19494b869
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2022-10-17 15:09:12 -0700

    Update elv.rb

--- elv.rb
+++ elv.rb
@@ -9,7 +9,7 @@ class Elv < Formula
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
         sha256 big_sur:     "e97af407a8d4c0f1509cd849ad62690ee8648d78bdfa74f45a38ce1a1b8879de"
         sha256 monterey:    "f37e19d2484877437b955e05bc0e1a1b3e6a051e634b85d80d9e3f6565f39814"
-#         sha256 arm64_big_sur: "44646e38a1df92fb73ca53cbaeb44348f54affa307f1208b6128ca4b1e2961e6"
+        sha256 arm64_big_sur: "5916ada83687b09c4aa4bb865e96408df284b8de436b5d4558c6f8619a1a4994"
     end
     
     def install
