cdcb908091cbd55f9c59a52f9b04be486a23caa8
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2022-12-12 12:27:45 -0800

    Update elv.rb

--- elv.rb
+++ elv.rb
@@ -2,14 +2,15 @@ class Elv < Formula
     
     desc "eluvio content fabric client"
     homepage "https://eluv.io"
-    VERSION = "0.2.13"
+    VERSION = "0.2.14"
     url "https://github.com/eluv-io/homebrew-eluv-io/archive/#{VERSION}.zip"
     
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
-        sha256 big_sur:     "e97af407a8d4c0f1509cd849ad62690ee8648d78bdfa74f45a38ce1a1b8879de"
-        sha256 monterey:    "f37e19d2484877437b955e05bc0e1a1b3e6a051e634b85d80d9e3f6565f39814"
-        sha256 arm64_big_sur: "2015178783e60d5351578713adc794fd4070ca4f61ba558d6b48da3d20cfe213"
+        sha256 big_sur:     "c4a4b217b473a566c8e396b2443c2a40de72cbe5c7a87350ebfbe9c339609d23"
+        sha256 monterey:    "59eaca41ed716f85b8b451b4830851dbb3852f23a5b0a387eeaba5ce016fda81"
+        sha256 ventura:     "553323372140f89077dc965540e8c2225af0d1664ee31c342031e1b61b72ab63"
+#         sha256 arm64_big_sur: "2015178783e60d5351578713adc794fd4070ca4f61ba558d6b48da3d20cfe213"
     end
     
     def install
