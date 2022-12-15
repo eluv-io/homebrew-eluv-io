5319073566688440a15c251b7712222b4f62f937
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2022-12-15 12:21:08 -0800

    Update elv.rb

--- elv.rb
+++ elv.rb
@@ -10,7 +10,7 @@ class Elv < Formula
         sha256 big_sur:     "c4a4b217b473a566c8e396b2443c2a40de72cbe5c7a87350ebfbe9c339609d23"
         sha256 monterey:    "59eaca41ed716f85b8b451b4830851dbb3852f23a5b0a387eeaba5ce016fda81"
         sha256 ventura:     "553323372140f89077dc965540e8c2225af0d1664ee31c342031e1b61b72ab63"
-        sha256 arm64_big_sur: "d766db1a37c5ef8d2c99bed92ec51110772c5da07ef41e54278b2c3e4b1331d5"
+        sha256 arm64_big_sur: "110b1e51c66c8326c7fe4ec28ba4a8bcfa5bb9e23bebff34b0a8a2a323caf3bc"
     end
     
     def install
