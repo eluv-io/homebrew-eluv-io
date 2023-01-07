648cbe399b8242317b4f138ceb449cd82041f08d
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2023-01-06 16:38:46 -0800

    Update elv.rb to support v0.3.0

--- elv.rb
+++ elv.rb
@@ -2,15 +2,15 @@ class Elv < Formula
     
     desc "eluvio content fabric client"
     homepage "https://eluv.io"
-    VERSION = "0.2.14"
+    VERSION = "0.3.0"
     url "https://github.com/eluv-io/homebrew-eluv-io/archive/#{VERSION}.zip"
     
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
-        sha256 big_sur:     "c4a4b217b473a566c8e396b2443c2a40de72cbe5c7a87350ebfbe9c339609d23"
-        sha256 monterey:    "59eaca41ed716f85b8b451b4830851dbb3852f23a5b0a387eeaba5ce016fda81"
-        sha256 ventura:     "553323372140f89077dc965540e8c2225af0d1664ee31c342031e1b61b72ab63"
-        sha256 arm64_big_sur: "46721b9760174d805589a6fd9917ab5674a2756d2bf3d2015281f04b9e259465"
+        sha256 big_sur:     "82f10c9ef3a038943cd38d14e86788f8348d1cee9a68a6b273bf24212680a88d"
+        sha256 monterey:    "4ce639ad5916ccb3616bb9a649973e78acef2f39cea8326cd0a120917737a72a"
+        sha256 ventura:     "456e4e2d3fc1832f3972b7def07c297c0f4d72215e66411afcfd0aae9d654a4c"
+#         sha256 arm64_big_sur: "46721b9760174d805589a6fd9917ab5674a2756d2bf3d2015281f04b9e259465"
     end
     
     def install
