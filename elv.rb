94d30120823c0b9155ffda200be0af2843953598
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2023-04-03 11:21:51 -0700

    Revert "Update elv.rb"
    This reverts commit 0dfd318e29ea109fa346a3628699fcd44ef5f4a5.

--- elv.rb
+++ elv.rb
@@ -2,15 +2,15 @@ class Elv < Formula
     
     desc "eluvio content fabric client"
     homepage "https://eluv.io"
-    VERSION = "0.3.1"
+    VERSION = "0.3.0"
     url "https://github.com/eluv-io/homebrew-eluv-io/archive/#{VERSION}.zip"
     
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
-        sha256 big_sur:     "4bc8ae612ff7397020ede71396f05c0b93f937baab7dbfcaa3f7bc6c082a570b"
-        sha256 monterey:    "d25d35df2e2a8bd2a34844a5825b7bd0f8c044d7662b0fe8ba27e4b08c683a31"
-        sha256 ventura:     "7e8518445efc9d4624594fd9199ed1eafd83c2bd459d6613e397ad6c353b402b"
-#         sha256 arm64_big_sur: "3114ab7b6b681642182b6f4201b8bd58534c92db43a7844d9f3cfee5c4eb2103"
+        sha256 big_sur:     "82f10c9ef3a038943cd38d14e86788f8348d1cee9a68a6b273bf24212680a88d"
+        sha256 monterey:    "4ce639ad5916ccb3616bb9a649973e78acef2f39cea8326cd0a120917737a72a"
+        sha256 ventura:     "456e4e2d3fc1832f3972b7def07c297c0f4d72215e66411afcfd0aae9d654a4c"
+        sha256 arm64_big_sur: "3114ab7b6b681642182b6f4201b8bd58534c92db43a7844d9f3cfee5c4eb2103"
     end
     
     def install
