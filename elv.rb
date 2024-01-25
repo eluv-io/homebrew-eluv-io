716a388be3db840c3404f85cb66c5a475dafe11d
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2024-01-24 18:38:42 -0800

    Update elv.rb

--- elv.rb
+++ elv.rb
@@ -11,6 +11,7 @@ class Elv < Formula
         sha256 ventura:     "5e103fc96a8b63ab4108033be0e07f188d18d93cfa7c166fee913939f89cea13"
         sha256 monterey:    "34d18473b0057fc1adb9f1a3fcbe339242c9b6d3899d95e122afd53eeed4b359"
         sha256 arm64_sonoma: "baad311b5a8e201c298e276b1b74d7f6409157dae49f6e5f5a3ab8b763a4c602"
+        sha256 arm64_ventura: "72c136d15eb5bb746eade272e819e2a10b9ab823db503ad167e0a32ca5091224"
     end
     
     def install
