7beeb71ca67ca313ff26d31aab9adedc77994f80
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2024-08-28 14:17:52 -0700

    Update elv.rb

--- elv.rb
+++ elv.rb
@@ -7,14 +7,14 @@ class Elv < Formula
     
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
-        sha256 sequoia:        "f3ea9fe8a1fad71e8b257af512f9ca212b85c6446deef32b4d1baa1f82abcefa"        
-        sha256 sonoma:         "9bc7f79136c588920f9d04b5422f4c12cd3ad85ba08c0b4ab1298e4e05562f1e"
-        sha256 ventura:        "dd27adacdd8b99caeb13280f0c6c644f085b13f082b7f43bab88903c8b4c706d"
-        sha256 monterey:       "886fe2e3104d9d2e652c5e1fe461d8dfaa71cb173d900249180ca2befe2b94f5"
-        sha256 arm64_sequoia:  "2bbc43ae7503eb2dc2e802be58250f609305edd5ef3d990e72df81b266f33f9b"     
-        sha256 arm64_sonoma:   "27be96ec7acf78d5358644939eb0e7c8f529a1292d13d3f2ff5bdd904d955fca"
-        sha256 arm64_ventura:  "14dd8aee111f62b10f249ed2bb61f678d934780f35bd97a07432fa990f0156a6"
-        sha256 arm64_monterey: "e8c3c8759a340375cd2d2c2cea3f77509812300b50488448f7b4f8678d73028d"   
+        sha256 sequoia:        "4ed0ceece41b0ed7d42c7b27e506d255030bbb47da9f99c533f149c778599ff1"        
+        sha256 sonoma:         "2fbc5a1203297862f44d28f87e5545285461ef5a81c82044916b8a396f2b73b2"
+        sha256 ventura:        "046dec5c62e597e4598f11542a607811594b9ebc9d2cc0af0072b7974b9a4a31"
+        sha256 monterey:       "f353f678cf5b01d5b1c548095f347501fea4a8f216a8d6589e2a9f8ed625b687"
+        sha256 arm64_sequoia:  "90fd409e16dd8d379bbe4292ab760a521caa5fb10d9a06a6e504893a61d893b5"     
+        sha256 arm64_sonoma:   "01b3882531591207b3b6cd7377b1174c97fbf91a0e1083886640300bb7e67cee"
+        sha256 arm64_ventura:  "091d0f11ebc67b87cae504eab0551e5ad3a9514a9bb2c37ddc35b4836fc78494"
+        sha256 arm64_monterey: "6cb35c680408c15bddd3555ed48c356ccec643afb2db250b297432d5f3d52560"   
     end
     
     def install
