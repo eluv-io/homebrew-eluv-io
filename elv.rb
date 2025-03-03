d1e4907832b408d1802e3e6e37c51af77310e7b2
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2025-03-03 13:53:08 -0800

    Update intel mac versions

--- elv.rb
+++ elv.rb
@@ -2,19 +2,19 @@ class Elv < Formula
     
     desc "eluvio content fabric client"
     homepage "https://eluv.io"
-    VERSION = "0.3.3"
+    VERSION = "0.3.4"
     url "https://github.com/eluv-io/homebrew-eluv-io/archive/#{VERSION}.zip"
     
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
-        sha256 sequoia:        "4ed0ceece41b0ed7d42c7b27e506d255030bbb47da9f99c533f149c778599ff1"        
-        sha256 sonoma:         "2fbc5a1203297862f44d28f87e5545285461ef5a81c82044916b8a396f2b73b2"
-        sha256 ventura:        "046dec5c62e597e4598f11542a607811594b9ebc9d2cc0af0072b7974b9a4a31"
-        sha256 monterey:       "f353f678cf5b01d5b1c548095f347501fea4a8f216a8d6589e2a9f8ed625b687"
-        sha256 arm64_sequoia:  "90fd409e16dd8d379bbe4292ab760a521caa5fb10d9a06a6e504893a61d893b5"     
-        sha256 arm64_sonoma:   "01b3882531591207b3b6cd7377b1174c97fbf91a0e1083886640300bb7e67cee"
-        sha256 arm64_ventura:  "091d0f11ebc67b87cae504eab0551e5ad3a9514a9bb2c37ddc35b4836fc78494"
-        sha256 arm64_monterey: "6cb35c680408c15bddd3555ed48c356ccec643afb2db250b297432d5f3d52560"   
+        sha256 sequoia:        "c0b8dca321ffe8d1b964e43e64c1f35bea14d74fb1ba414ed6b335e3260d1ce2"        
+        sha256 sonoma:         "9e75f14f39e3cf7f83f3f7b7c832fb4a2c0a93c874661111ff5b2eba24abfc93"
+        sha256 ventura:        "3e691c22b8c9c67b58f0cd28eee2ba935f1acafab469c0c11aa95fbbdcd1c749"
+        sha256 monterey:       "19a7b746f6573eec50302be5aebf06c5f1456d687b41eed47af5f82d218bb7d6"
+        sha256 arm64_sequoia:  ""     
+        sha256 arm64_sonoma:   ""
+        sha256 arm64_ventura:  ""
+        sha256 arm64_monterey: ""   
     end
     
     def install
