75d2ea7d8b8085d44f4cda5257550d2896072fea
elv-preethi <39742400+elv-preethi@users.noreply.github.com>
2026-04-02 13:11:47 -0700

    Update version and SHA256 checksums for elv.rb

--- elv.rb
+++ elv.rb
@@ -2,19 +2,19 @@ class Elv < Formula
     
     desc "eluvio content fabric client"
     homepage "https://eluv.io"
-    VERSION = "0.3.4"
+    VERSION = "4.4.11"
     url "https://github.com/eluv-io/homebrew-eluv-io/archive/#{VERSION}.zip"
     
     bottle do
         root_url "https://github.com/eluv-io/homebrew-eluv-io/releases/download/#{VERSION}"
-        sha256 sequoia:        "c0b8dca321ffe8d1b964e43e64c1f35bea14d74fb1ba414ed6b335e3260d1ce2"        
-        sha256 sonoma:         "9e75f14f39e3cf7f83f3f7b7c832fb4a2c0a93c874661111ff5b2eba24abfc93"
-        sha256 ventura:        "3e691c22b8c9c67b58f0cd28eee2ba935f1acafab469c0c11aa95fbbdcd1c749"
-        sha256 monterey:       "19a7b746f6573eec50302be5aebf06c5f1456d687b41eed47af5f82d218bb7d6"
-        sha256 arm64_sequoia:  "31c71ecc3db64689859701ca84966f43e10de8c2e266f4b6ae99483a0c99dadd"     
-        sha256 arm64_sonoma:   "55249999e7e8df1deafecb04adc8ddfcc93d8bf2be877461055f9a55398aec14"
-        sha256 arm64_ventura:  "e726c48f3e006560f532f1ee4620e7126670e6c20d5b53aef5ad48a466cfe7b5"
-        sha256 arm64_monterey: "cf5e207ce3c6e07fe2f895250c3b238f580c2420ce5e055ac3cc8b630a110b3d"   
+        sha256 tahoe:          "7c5f9bc1a62f29b477164289895ba7076979bf358b7cc2d45ce8b5d261deaaa4"
+        sha256 sequoia:        "3801968cfb604f3ec47292a6c3d53de27a1c6cde3872791112875af1ae094b61"        
+        sha256 sonoma:         "1fdc7c7d13fe0b0211685e03c0dfbdf16f241b652f97dc456c773ecf214d5987"
+        sha256 ventura:        "70180244bbfabc12d0ba5a7a49ad56d8472e8d454352e05f0f22ba476997959e"
+        sha256 arm64_tahoe:    "87eabede043cacc7ac1e5c8e902a9cee00af6118206fa84f2b44fe356d5ac75e"
+        sha256 arm64_sequoia:  "2a76cdf720075f0a1eaac95bf4da6f809eb6943767279552e33e07c23cd01ddc"     
+        sha256 arm64_sonoma:   "f2846e45900f0f12acfd02ee9b0d2a13735a23cb8209df8a8cc04fc3f23138fb"
+        sha256 arm64_ventura:  "d7c511b347ec48899d593d9f2b77b4c6252b0e7cd2e5af3dd8fc9316a3bff7cc"
     end
     
     def install
